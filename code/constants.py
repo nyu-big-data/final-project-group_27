7d29bae2c298e66766631af537c2227e97ea61c5
Gduregon <gjd9961@nyu.edu>
2022-04-21 19:33:37 -0400

    mroe bugs

--- code/constants.py
+++ code/constants.py
@@ -2,8 +2,8 @@
 A python file to import constants that point to data sources
 """
 HPC_DATA_FILEPATH = "/scratch/work/courses/DSGA1004-2021/movielens/"
-CSV_NAME_LIST = ['/movies.csv','/ratings.csv','/tags.csv','/links.csv']
-SMALL = 'hm-latest-small'
-LARGE = 'hm-latest'
+CSV_NAME_LIST = ['movies.csv','ratings.csv','tags.csv','links.csv']
+SMALL = 'hm-latest-small/'
+LARGE = 'hm-latest/'
 DATASET_DICT = {'small': HPC_DATA_FILEPATH+SMALL,
                  'large': HPC_DATA_FILEPATH+LARGE}
