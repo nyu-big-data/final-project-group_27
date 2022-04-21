9e873fc2c91c6babb37c727f10f779af25559471
Gduregon <gjd9961@nyu.edu>
2022-04-21 00:50:45 -0400

    Tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='code',
     url='https://github.com/nyu-big-data/final-project-group_27/tree/main/Code',
     author='Giulio Duregon, Joby George, Jonah P',
-    author_email='gjd9961@nyuledu',
+    author_email='gjd9961@nyu.edu',
     # Needed to actually package something
-    packages=['code'],
-    # Needed for dependencies
-    install_requires=['numpy'],
+    packages=find_packages(),
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
-    license='NYU',
-    description='Code Package for Big Data Final Project',
+    description='Code Package for Big Data Final Project'
     # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    #,long_description=open('README.md').read(),
 )
