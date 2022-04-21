9518bb76779e972364612d72eaaf8ba1e046024a
Gduregon <gjd9961@nyu.edu>
2022-04-21 16:08:51 -0400

    Trying to fix bugs

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-export PYTHONPATH=/${SCRATCH}/final-project-group_27/code/
+export PYTHONPATH=/${SCRATCH}/final-project-group_27/
 export PROJECT_ROOT=$(pwd)
 export HADOOP_EXE='/usr/bin/hadoop'
 module load python/gcc/3.7.9
 module load spark/3.0.1
-
 alias hfs="$HADOOP_EXE fs"
 alias spark-submit='PYSPARK_PYTHON=$(which python) spark-submit'
