039757cc158f8d458ddc6fe79a010125a91a84da
Gduregon <gjd9961@nyu.edu>
2022-04-21 19:29:43 -0400

    More small bugs

--- code/constants.py
+++ code/constants.py
@@ -2,7 +2,7 @@
 A python file to import constants that point to data sources
 """
 HPC_DATA_FILEPATH = "/scratch/work/courses/DSGA1004-2021/movielens/"
-CSV_NAME_LIST = ['movies.csv','ratings.csv','tags.csv','links.csv']
+CSV_NAME_LIST = ['/movies.csv','/ratings.csv','/tags.csv','/links.csv']
 SMALL = 'hm-latest-small'
 LARGE = 'hm-latest'
 DATASET_DICT = {'small': HPC_DATA_FILEPATH+SMALL,
