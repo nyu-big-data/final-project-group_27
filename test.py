098e514547bb34585c724edea1a0d08d4015da8b
Gduregon <gjd9961@nyu.edu>
2022-04-21 19:44:55 -0400

    Small test

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from pyspark.sql import SparkSession
 
 spark = spark = SparkSession.builder.appName('Spark_Session_Name').getOrCreate()
 
-file_path = "/scratch/work/courses/DSGA1004-2021/movielens/ml-latest-small/"
-spark.read.csv(file_path + 'movies.csv', schema='movieId INT, title STRING, genres STRING')
+file_path = "/scratch/work/courses/DSGA1004-2021/movielens/ml-latest-small/movies.csv"
+spark.read.csv(file_path, schema='movieId INT, title STRING, genres STRING')
 print("Sucess")
