4b42d14982e6785e1dc8bbe58e59b9318edcc71c
Gduregon <gjd9961@nyu.edu>
2022-04-21 19:36:37 -0400

    stupid type

--- code/constants.py
+++ code/constants.py
@@ -3,7 +3,7 @@ A python file to import constants that point to data sources
 """
 HPC_DATA_FILEPATH = "/scratch/work/courses/DSGA1004-2021/movielens/"
 CSV_NAME_LIST = ['movies.csv','ratings.csv','tags.csv','links.csv']
-SMALL = 'hm-latest-small/'
-LARGE = 'hm-latest/'
+SMALL = 'ml-latest-small/'
+LARGE = 'ml-latest/'
 DATASET_DICT = {'small': HPC_DATA_FILEPATH+SMALL,
                  'large': HPC_DATA_FILEPATH+LARGE}
