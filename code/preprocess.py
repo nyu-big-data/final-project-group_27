6c30e77a2ffa31d4c388e89b94dfe5b05bb7e0d0
Gduregon <gjd9961@nyu.edu>
2022-04-21 20:07:39 -0400

    Didn't like DATETYPE format

--- code/preprocess.py
+++ code/preprocess.py
@@ -42,7 +42,7 @@ class DataPreprocessor():
         movies = self.spark.read.csv(self.file_path + 'movies.csv', schema='movieId INT, title STRING, genres STRING')
     
         #Same for ratings
-        ratings = self.spark.read.csv(self.file_path + 'ratings.csv', schema='userId INT, movieId INT, rating FLOAT, timestamp DATETYPE') #Date type?
+        ratings = self.spark.read.csv(self.file_path + 'ratings.csv', schema='userId INT, movieId INT, rating FLOAT, timestamp DATE') #Date type?
         
         #Combine or ditch reviews that have dupes?
         double_counts = movies.groupby('title').count()
