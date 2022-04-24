c40f7cbb9a2125ba206783ffd696bd67105467a9
Gduregon <gjd9961@nyu.edu>
2022-04-24 17:55:14 -0400

    Fixing filepath error

--- code/constants.py
+++ code/constants.py
@@ -3,6 +3,6 @@ A python file to import constants that point to data sources
 """
 import getpass
 netID = getpass.getuser() + "/"
-HPC_DATA_FILEPATH = "hdfs:/user/" + netID
-MODEL_SAVE_FILE_PATH = netID + "scratch/rec_models/"
-RESULTS_SAVE_FILE_PATH = "/gjd9961/scratch/big_data_final_results/results.txt"   
+HPC_DATA_FILEPATH = f"hdfs:/user/{netID}"
+MODEL_SAVE_FILE_PATH = f"scratch/{netID}rec_models/"
+RESULTS_SAVE_FILE_PATH = f"/scratch/{netID}big_data_final_results/results.txt"   
