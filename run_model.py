bb3a4c5775662f2c25829fe977882605ccf0c413
Gduregon <gjd9961@nyu.edu>
2022-04-26 00:04:33 -0400

    More refactoring: :

--- run_model.py
+++ run_model.py
@@ -4,40 +4,42 @@ import code.constants as const
 import sys
 import json
 
-#Main Function that will define model behavior
-def main(spark, model_size,model_type,model_args):
+# Main Function that will define model behavior
 
-    #Grab the filepaths for model_size
+
+def main(spark, model_size, model_type, model_args):
+
+    # Grab the filepaths for model_size
     print(f"Getting train / test / val splits for {model_size}")
-    train_file_path = const.HPC_DATA_FILEPATH + model_size + "train.csv"
-    test_file_path = const.HPC_DATA_FILEPATH + model_size + "test.csv"
-    val_file_path = const.HPC_DATA_FILEPATH + model_size + "val.csv"
-    
-    #Read data for file paths
-    train = spark.read.csv(train_file_path, header=True, \
-                            schema='rating DOUBLE, userId INT, movieId INT, date STRING, title STRING, row_num INT, length INT')
-    test = spark.read.csv(test_file_path, header=True, \
-                            schema = 'rating DOUBLE, userId INT, movieId INT, date STRING, title STRING, row_num INT, length INT') 
-    val = spark.read.csv(val_file_path, header=True, \
-                            schema='rating DOUBLE, userId INT, movieId INT, date STRING, title STRING, row_num INT, length INT')
+    train_file_path = f"{const.HPC_DATA_FILEPATH}{model_size}-train.csv"
+    test_file_path = f"{const.HPC_DATA_FILEPATH}{model_size}-test.csv"
+    val_file_path = f"{const.HPC_DATA_FILEPATH}{model_size}-val.csv"
+
+    # Read data for file paths
+    train = spark.read.csv(train_file_path, header=True,
+                           schema=const.TRAIN_VAL_TEST_SCHEMA)
+    test = spark.read.csv(test_file_path, header=True,
+                          schema=const.TRAIN_VAL_TEST_SCHEMA)
+    val = spark.read.csv(val_file_path, header=True,
+                         schema=const.TRAIN_VAL_TEST_SCHEMA)
     print("Data Read Successfully")
 
     print(f"Args being passed to model: {model_args.items()}")
-    #Pass through dictionary of keyword arguments to Model()
-    reccomender_system = Model(**model_args)
-    
-    #Run model according to what model_type parameter was passed
-    #If model is Alternating Least Squares use ALS_fit_and_run
-    if model_type == 'als':
-        #IMPORTANT: I HAVE THIS CURRENTLY ONLY PREICTING AND MEASURING ERROR ON VAL - WHEN WE'RE DONE TUNING AND EVALUATING
-        #RESULTS WE'LL HAVE TO GO INTO Als_fit_and_run AND CHANGE THE CODE!
-        userRecs, movieRecs = reccomender_system.ALS_fit_and_predict(training=train,val=val)
+    # Pass through dictionary of keyword arguments to Model()
+    reccomender_system = Model(
+        model_size=model_size, model_type=model_type, **model_args)
+    # Run the model
+    reccomender_system.run_model(train, val)
+    # Write our results and model parameters
+    print("Recording the model_params")
+    with open(reccomender_system.results_file_path, 'a') as output_file:
+        # Write each element of metrics seperated by a comma, for the last one use a new line character
+        for i in range(len(model_args)-1):
+            output_file.write(f"{model_args[i]},")
+        output_file.write(f"{model_args[-1]}\n")
 
-    #If baseline... you guessed it, use baseline
-    elif model_type == 'baseline':
-        movieRecs = reccomender_system.baseline(train,val,test)
 
-
+# Enter this block if we're in __main__
 if __name__ == '__main__':
     """
     Model argumensts hsould be entered in the following order:
@@ -46,12 +48,21 @@ if __name__ == '__main__':
     3) Dictionary of {parameter:argument} pairs that will be parsed to the model
     i.e. '{"rank":10, "maxIter":10,"regParam":0.05}'
     """
-    #Initialize spark context
+    # Initialize spark context
     spark = SparkSession.builder.appName('Run_Model').getOrCreate()
-    #Model size is either "small" or "large"
-    model_size = sys.argv[1]+"-"
-    #Define the model type in second argument:
+    # Model size is either "small" or "large"
+    model_size = sys.argv[1]
+    # Define the model type in second argument:
     model_type = sys.argv[2]
-    #Model Args:
+    # Model Args:
     model_args = json.loads(sys.argv[3])
-    main(spark, model_size,model_type,model_args)
+
+    # Make sure input is valid
+    if model_size not in ['small', 'large']:
+        raise Exception(
+            f"Model Size must either be 'small' or 'large', you entered {model_size}")
+    if model_type not in ['als', 'baseline']:
+        raise Exception(
+            f"Model Type must be either 'als' or 'baseline', you entered {model_type}")
+    #Call Main
+    main(spark, model_size, model_type, model_args)
