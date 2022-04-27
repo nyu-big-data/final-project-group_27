f24fc4120bb9ac2d3cb6ad071e511a09f9d56fa9
Gduregon <gjd9961@nyu.edu>
2022-04-27 15:32:39 -0400

    test.py

--- test.py
+++ test.py
@@ -1,13 +1,30 @@
-
+from pyspark.sql import SparkSession
+from code.model import Model
+import code.constants as const
 import sys
 import json
 
-#Main Function that will define model behavior
-def main(model_size,model_type,model_args):
-
-    print(model_size, model_type, model_args)
+# Main Function that will define model behavior
 
 
+def main(spark, model_size, model_type, model_args):
+
+    # Grab the filepaths for model_size
+    train_file_path = f"{const.HPC_DATA_FILEPATH}{model_size}-train.csv"
+    test_file_path = f"{const.HPC_DATA_FILEPATH}{model_size}-test.csv"
+    val_file_path = f"{const.HPC_DATA_FILEPATH}{model_size}-val.csv"
+
+    # Read data for file paths
+    train = spark.read.csv(train_file_path, header=True,
+                           schema=const.TRAIN_VAL_TEST_SCHEMA)
+    test = spark.read.csv(test_file_path, header=True,
+                          schema=const.TRAIN_VAL_TEST_SCHEMA)
+    val = spark.read.csv(val_file_path, header=True,
+                         schema=const.TRAIN_VAL_TEST_SCHEMA)
+
+    print(f"Train Count: {train.count()}, Test Count {test.count()}, Val Count: {val.count()}")
+
+# Enter this block if we're in __main__
 if __name__ == '__main__':
     """
     Model argumensts hsould be entered in the following order:
@@ -16,11 +33,21 @@ if __name__ == '__main__':
     3) Dictionary of {parameter:argument} pairs that will be parsed to the model
     i.e. '{"rank":10, "maxIter":10,"regParam":0.05}'
     """
-    #Model size is either "small" or "large"
-    model_size = sys.argv[1]+"_"
-    #Define the model type in second argument:
+    # Initialize spark context
+    spark = SparkSession.builder.appName('Run_Model').getOrCreate()
+    # Model size is either "small" or "large"
+    model_size = sys.argv[1]
+    # Define the model type in second argument:
     model_type = sys.argv[2]
-    #Model Args:
-    params = sys.argv[3]
-    model_args = json.loads(params)
-    main(model_size,model_type,model_args)
+    # Model Args:
+    model_args = json.loads(sys.argv[3])
+
+    # Make sure input is valid
+    if model_size not in ['small', 'large']:
+        raise Exception(
+            f"Model Size must either be 'small' or 'large', you entered {model_size}")
+    if model_type not in ['als', 'baseline']:
+        raise Exception(
+            f"Model Type must be either 'als' or 'baseline', you entered {model_type}")
+    #Call Main
+    main(spark, model_size, model_type, model_args)
