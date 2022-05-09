7f4afac9f640c207fadf3a973a26d8944eaaaf7e
Gduregon <gjd9961@nyu.edu>
2022-05-08 22:17:52 -0400

    Maybe thi'll work

--- HP_search_als.py
+++ HP_search_als.py
@@ -9,7 +9,7 @@ import numpy as np
 # Main Function that will define model behavior
 
 
-def main(spark, model_size, maxIters, ranks, regParams):
+def main(spark, model_size, ranks):
 
     # Grab the filepaths for model_size
     train_file_path = f"{const.HPC_DATA_FILEPATH}als-{model_size}-train.csv"
@@ -21,31 +21,32 @@ def main(spark, model_size, maxIters, ranks, regParams):
     val = spark.read.csv(val_file_path, header=True,
                          schema=const.VAL_TEST_SCHEMA)
 
+    
     #Iterate over K values - Calculate Baseline Model and Search for best K on val performance
-    for maxIter in maxIters:
-        for rank in ranks:
-            for regParam in regParams:
-                print(f"Running Model: {maxIter}, {rank}, {regParam}")
-                # Pass through dictionary of keyword arguments to Model()
-                reccomender_system = Model(model_size=model_size, model_type='als', rank=int(rank), maxIter = int(maxIter),regParam = float(regParam))
-                # Run the model
-                reccomender_system.run_model(train, val)
+    regParams = np.logspace(-3,-1,5)
+    for rank in ranks:
+        for regParam in regParams:
+            print(f"Running Model: Max Iter:{10}, Rank: {rank}, Reg Param: {regParam}")
+            # Pass through dictionary of keyword arguments to Model()
+            reccomender_system = Model(model_size=model_size, model_type='als', rank=int(rank), maxIter = 10,regParam = float(regParam))
+            # Run the model
+            reccomender_system.run_model(train, val)
 
-                #Grab the key:value pairs of instance variables
-                instance_vars = vars(reccomender_system)
-                print(f'instance vars: {instance_vars}')
-                #Get rid of "methods" nested dict - it has the function calls which can't be written to output file
-                del instance_vars["methods"]
+            #Grab the key:value pairs of instance variables
+            instance_vars = vars(reccomender_system)
+            print(f'instance vars: {instance_vars}')
+            #Get rid of "methods" nested dict - it has the function calls which can't be written to output file
+            del instance_vars["methods"]
 
-                print("Writing results")
-                # Write our results and model parameters
-                print("Recording the model_params")
-                with open(const.RESULTS_SAVE_FILE_PATH, 'a') as output_file:
-                    output_file.write(json.dumps(instance_vars))
-                    output_file.write("\n")
+            print("Writing results")
+            # Write our results and model parameters
+            print("Recording the model_params")
+            with open(const.RESULTS_SAVE_FILE_PATH, 'a') as output_file:
+                output_file.write(json.dumps(instance_vars))
+                output_file.write("\n")
 
-                #Clear anything thats cached
-                spark.catalog.clearCache()
+            #Clear anything thats cached
+            spark.catalog.clearCache()
 
 # Enter this block if we're in __main__
 if __name__ == '__main__':
@@ -56,19 +57,13 @@ if __name__ == '__main__':
     spark = SparkSession.builder.appName('proj').getOrCreate()
     # Model size is either "small" or "large"
     model_size = sys.argv[1]
-    maxIters_start = int(sys.argv[2])
-    maxIters_stop = int(sys.argv[3])
-    ranks_start = int(sys.argv[4])
-    ranks_stop = int(sys.argv[5])
-    regParams_start = int(sys.argv[6])
-    regParams_stop = int(sys.argv[7])
+    ranks_start = int(sys.argv[2])
+    ranks_stop = int(sys.argv[3])
 
-    maxIters = np.linspace(maxIters_start, maxIters_stop, num=5)
-    ranks = np.linspace(ranks_start,ranks_stop, num=5)
-    regParams = np.logspace(regParams_start,regParams_stop, 5)
+    ranks = range(ranks_start, ranks_stop, step=2)
 
     # Make sure input is valid
     if model_size not in ['small', 'large']:
         raise Exception(f"Model Size must either be 'small' or 'large', you entered {model_size}")
     #Call Main
-    main(spark, model_size, maxIters, ranks, regParams)
+    main(spark, model_size, ranks)
